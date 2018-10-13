8333f190484e3185d2ef94dbdfffc30241dcfa07
Nishtha Bhatia <nbhatia329@gmail.com>
2018-10-13 18:27:58 +0530

    Single book layout, adapter

--- app/src/main/java/com/apps/bellatrix/booketh/MainActivity.java
+++ app/src/main/java/com/apps/bellatrix/booketh/MainActivity.java
@@ -1,7 +1,10 @@
 package com.apps.bellatrix.booketh;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.menuProfile){
+            Intent intent = new Intent(MainActivity.this, WriterProfileActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return false;
     }
 }
