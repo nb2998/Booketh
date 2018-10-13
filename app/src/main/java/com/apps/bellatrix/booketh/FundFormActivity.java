90776dca168a5a6bd3998c6999057dbdfc16bdbc
dilsheen <dilsheenkaur98@gmail.com>
2018-10-14 02:21:27 +0530

    UI Changes and fund form added

---
+++ app/src/main/java/com/apps/bellatrix/booketh/FundFormActivity.java
@@ -0,0 +1,13 @@
+package com.apps.bellatrix.booketh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FundFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fund_form);
+    }
+}
