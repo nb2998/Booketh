388fc12d9a181bd73062dac6698dcd0439a54def
dilsheen <dilsheenkaur98@gmail.com>
2018-10-13 20:29:32 +0530

    Added AddBook, BookDetails, CreateBook, FundProject Activities

---
+++ app/src/main/java/com/apps/bellatrix/booketh/AddBookActivity.java
@@ -0,0 +1,13 @@
+package com.apps.bellatrix.booketh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_book);
+    }
+}
