58561d90009be7812940d8b25bbd4479859196fe
dilsheen <dilsheenkaur98@gmail.com>
2018-10-14 07:22:31 +0530

    AdvertiseActivity Added

--- app/src/main/java/com/apps/bellatrix/booketh/AdvertiseActivity.java
+++ app/src/main/java/com/apps/bellatrix/booketh/AdvertiseActivity.java
@@ -2,12 +2,35 @@ package com.apps.bellatrix.booketh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 
 public class AdvertiseActivity extends AppCompatActivity {
-
+    Spinner spPlan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advertise);
+        spPlan= (Spinner) findViewById(R.id.spPlan);//fetch the spinner from layout file
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, getResources()
+                .getStringArray(R.array.country_array));//setting the country_array to spinner
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spPlan.setAdapter(adapter);
+//if you want to set any action you can do in this listener
+        spPlan.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1,
+                                       int position, long id) {
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+            }
+        });
+
     }
 }
