ad306f229447b7d9cb68e0ee4812a864ded6a547
Nishtha Bhatia <nbhatia329@gmail.com>
2018-10-14 12:07:53 +0530

    Advertise layout

--- app/src/main/java/com/apps/bellatrix/booketh/AdvertiseActivity.java
+++ app/src/main/java/com/apps/bellatrix/booketh/AdvertiseActivity.java
@@ -9,28 +9,31 @@ import android.widget.Spinner;
 
 
 public class AdvertiseActivity extends AppCompatActivity {
-    Spinner spPlan;
+//    Spinner spPlan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advertise);
-        spPlan= (Spinner) findViewById(R.id.spPlan);//fetch the spinner from layout file
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, getResources()
-                .getStringArray(R.array.country_array));//setting the country_array to spinner
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spPlan.setAdapter(adapter);
-//if you want to set any action you can do in this listener
-        spPlan.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> arg0, View arg1,
-                                       int position, long id) {
-            }
+//        spPlan= (Spinner) findViewById(R.id.spPlan);//fetch the spinner from layout file
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+//                android.R.layout.simple_spinner_item, getResources()
+//                .getStringArray(R.array.country_array));//setting the country_array to spinner
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spPlan.setAdapter(adapter);
+////if you want to set any action you can do in this listener
+//        spPlan.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> arg0, View arg1,
+//                                       int position, long id) {
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> arg0) {
+//            }
+//        });
 
-            @Override
-            public void onNothingSelected(AdapterView<?> arg0) {
-            }
-        });
+
+
 
     }
 }
